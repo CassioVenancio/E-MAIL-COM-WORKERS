7388eb6d61655a986d1fada5689e152bf69ca91e
Cássio Venâncio <cassiolvenancio@gmail.com>
2022-03-06 15:04:02 -0300

    Finalização do projeto

--- app/sender.py
+++ app/sender.py
@@ -1,15 +1,22 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'host=db dbname=email_sender user=postgres password=postgres'
-        self.conn = psycopg2.connect(DSN)
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_password = os.getenv('DB_PASSWORD', 'postgres')
+        dsn = f'dbname={db_name} user={db_user} host={db_host} password={db_password}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
